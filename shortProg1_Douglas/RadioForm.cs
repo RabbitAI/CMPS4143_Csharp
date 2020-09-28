f7320c12be74e7037caac1c7e2c77e4e95c4508b
Christian Douglas <cedoug13@aol.com>
2020-09-27 22:41:35 -0500

    Updating files

---
+++ shortProg1_Douglas/RadioForm.cs
@@ -0,0 +1,68 @@
+﻿/*
+ *          Name: Christian Douglas
+ *          Date: August 31, 2020
+ *          Description: The application purpose is to have the
+ *          appearance of a radio, using the tools in the toolbox
+ *          section and following the steps in the textbook, four
+ *          group boxes were created. The first group box is called
+ *          Pre-set Stations and has 6 buttons in it labeled 1 
+ *          through 6 to simulate the presets a radio has. Group
+ *          box 2 is labled Speakers and contains two check boxes
+ *          labeled front and rear to simulate the control of 
+ *          choosing which speakers to use. To the right of the
+ *          second group box is a text box with Power On/Off to 
+ *          simulate a power button. Group box 3 is at the bottom
+ *          left and is labeled Volume Control and contains a Mute
+ *          check box to simulate the mute functin and a track bar 
+ *          to simulate the control of the volume for the radio app.
+ *          Group box 4 is labeled Tuning and contains a label with
+ *          a black background with silver text saying 92.9 and 2 
+ *          radio buttons labeled AM and FM to simulate the frequency 
+ *          types. Next to group box for is a picture box with a gif
+ *          type image of a music note in it.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shortProg1
+{
+    public partial class RadioForm : Form
+    {
+        public RadioForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
